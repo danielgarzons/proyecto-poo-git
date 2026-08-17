319e95d216f4665b5b0852ae07b3fc87b91942a8
Juan Daniel Garzon Solaque <danielgarzonjuan12@gmail.com>
2026-08-16 19:18:03 -0500

    feat: agrega constructor clase Tarea

--- src/gestionproyectos/Tarea.java
+++ src/gestionproyectos/Tarea.java
@@ -1,3 +1,12 @@
+
+// ----- 1. CREE LA CLASE ESTUDIANTE -----
 public class Estudiante { 
-    
+// ---- 2. LOS ATRIBUTOS DE LA CLASE: ESTUDIANTE -----
+    String nombre;
+    int edad;
+// ----- 3. CONSTRUCTOR LISTO PARA USO ------
+    public Estudiante(String nombre, int edad){
+         this.nombre = nombre;
+         this. edad = edad; 
+    }
 }
