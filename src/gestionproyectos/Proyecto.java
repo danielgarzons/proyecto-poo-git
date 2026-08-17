5760cadb2f5b86ea2014e442c37c9bf1648b146a
Juan Daniel Garzon Solaque <danielgarzonjuan12@gmail.com>
2026-08-16 19:26:18 -0500

    feat: agrega constructor clase Proyecto

--- src/gestionproyectos/Proyecto.java
+++ src/gestionproyectos/Proyecto.java
@@ -1,3 +1,9 @@
 public class Estudiante {
-    
+     String nombre;
+     int edad;
+
+     public Estudiante (String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+     }
 }
