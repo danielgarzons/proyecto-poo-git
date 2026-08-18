3ad8c45e0c018bf5d42b48db1d13417f5cc05bbf
Juan Daniel Garzon Solaque <danielgarzonjuan12@gmail.com>
2026-08-17 19:57:25 -0500

    feat: agrega método Recuperar vida jugador Mago

--- Mago.java
+++ Mago.java
@@ -4,10 +4,18 @@ public class Mago {
     String habilidadespecialmago;
     int vidamago;
 
+    String jugadorvidasubida;
+    int cantidadvidasubida;
+    
+
     public Lanzarfuego (String nombremago, String habilidadespecialmago, int vidamago){
         this.nombremago = nombremago;
         this.habilidadespecialmago = habilidadespecialmago;
         this.vidamago = vidamago;
     }
 
+    public RecuperarVidaJugador (String jugadorvidasubida, int cantidadvidasubida){
+        this.jugadorvidasubida = jugadorvidasubida;
+        this.cantidadvidasubida = cantidadvidasubida;
+    }
 }
