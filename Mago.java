4f17fd9b54ca42afaae6aee90f412d38505ecd03
Juan Daniel Garzon Solaque <danielgarzonjuan12@gmail.com>
2026-08-17 19:51:28 -0500

    feat: agrega clase Mago, método lanzar fuego Mago

---
+++ Mago.java
@@ -0,0 +1,13 @@
+public class Mago {
+    
+    String nombremago;
+    String habilidadespecialmago;
+    int vidamago;
+
+    public Lanzarfuego (String nombremago, String habilidadespecialmago, int vidamago){
+        this.nombremago = nombremago;
+        this.habilidadespecialmago = habilidadespecialmago;
+        this.vidamago = vidamago;
+    }
+
+}
