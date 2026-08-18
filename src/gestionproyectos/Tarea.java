2b3a14f7d8fe38354cbd9a51520656606bcccac3
Juan Daniel Garzon Solaque <danielgarzonjuan12@gmail.com>
2026-08-17 19:25:24 -0500

    refactor: cambio clase Estudiante -> Tarea

--- src/gestionproyectos/Tarea.java
+++ src/gestionproyectos/Tarea.java
@@ -1,12 +1,13 @@
 
-// ----- 1. CREE LA CLASE ESTUDIANTE -----
-public class Estudiante { 
-// ---- 2. LOS ATRIBUTOS DE LA CLASE: ESTUDIANTE -----
-    String nombre;
-    int edad;
-// ----- 3. CONSTRUCTOR LISTO PARA USO ------
-    public Estudiante(String nombre, int edad){
-         this.nombre = nombre;
-         this. edad = edad; 
+public class Tarea { 
+
+    String Titulo;
+    Date fechadeentrega;
+    boolean hizolatarea;
+
+    public Estudiante(String Titulo, Date fechadeentrega; boolean hizolatarea;){
+         this.Titulo = nombre;
+         this.fechdeentrega = edad;
+         this.hizolatarea = hizolatarea; 
     }
 }
